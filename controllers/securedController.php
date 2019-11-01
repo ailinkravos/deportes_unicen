4db8bfa54208d6a011751a0ea3130e0f2d775310
Ailin Kravos <ailinkravos@gmail.com>
2019-11-01 15:18:44 -0300

    subo modificaciones de loginView y securedController

--- controllers/securedController.php
+++ controllers/securedController.php
@@ -1,13 +1,15 @@
 <?php
 define('LOGIN', 'http://'.$_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']).'/login');
 define('LOGOUT', 'http://'.$_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']).'/logout');
+
 class SecuredController {
-    public function __construct() {
+    public function __construct(){
         session_start();
         // verifica que este logueado
         if(!isset($_SESSION['username'])){
           header('Location: login');
           die();
+        }
         if(isset($_SESSION['USERNAME'])){ // si esta logueado
             if (time() - $_SESSION['LAST_ACTIVITY'] > 10) { // expiro el timeout
                 header('Location: '.LOGOUT);
@@ -18,6 +20,7 @@ class SecuredController {
         else {
             header('Location: '. LOGIN);
             die();
-          }
+        }
+    
     }
 }
