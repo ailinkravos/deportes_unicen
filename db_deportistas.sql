ac410dc8eb7cf363a2caa91ab287f61ab5bf1765
Ayelen Diaz Bailey <vdiazbailey@alumnos.exa.unicen>
2019-10-31 22:21:44 -0300

    Subo la bbdd

--- db_deportistas.sql
+++ db_deportistas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-10-2019 a las 22:53:40
--- Versión del servidor: 10.4.6-MariaDB
--- Versión de PHP: 7.3.8
+-- Tiempo de generación: 31-10-2019 a las 22:59:08
+-- Versión del servidor: 10.1.26-MariaDB
+-- Versión de PHP: 7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -110,30 +110,18 @@ CREATE TABLE `facultad` (
 --
 
 INSERT INTO `facultad` (`id_facultad`, `nombre_facultad`, `sede`, `historia`) VALUES
-(1, 'Agronomía', 'Azul', 'Por el momento no tienen una mascota definida.\r\n\r\nLos colores que utilizan son el bordó y el blanco.'),
-(2, 'Arte', 'Tandil', 'Adoptaron como mascota al arlequín.\r\n\r\nSus colores varían entre las tonalidades del rojo. Sus camisetas suelen ser color azul violáceo y rojo.\r\n\r\n'),
-(3, 'Derecho', 'Azul', 'Esta facultad si bien no tiene del todo definida su mascota, el logo de las remeras de sus equipos es un cuervo. Los jugadores decidieron poner esta mascota porque a los abogados se los suele identificar con ese animal.\r\n\r\nLos colores con lo que los vas a poder identificar son el magenta y el azul.'),
-(5, 'Económicas', 'Tandil', 'Según cuenta la leyenda, los actuales contadores Gabriel Dadiego y Mario Cabitto fueron designados para establecer el apodo del Centro de estudiantes y los colores que se usarían en olimpiadas.\r\n\r\nEllos, hinchas de Estudiantes de La Plata uno y de Racing Club el otro, hicieron ostentación de esa condición para elegir el apodo de \"León\" y los colores celeste y blanco.\r\n\r\n'),
-(6, 'Exactas', 'Tandil', 'Según el boca en boca, la facultad de Exactas es representada por el gallo debido a que hace mucho tiempo, veterinarias hizo una canción llamándoles gallinas y ellos les retrucaron con otra canción autoproclamándose gallos, alegando a la fiereza combativa de dicho animal.\r\n\r\nEn cuanto a los colores, la historia se remonta a las primeras olimpiadas donde no existía ningún tipo de presupuesto para las remeras. Ante esto, el Club Santamarina les prestó remeras suyas de color oro y negro.\r\n\r\n'),
-(7, 'Humanas', 'Tandil', 'Su mascota es Boxitracio, un personaje inventado por García Ferré para la serie Hijitus. El prefijo “Boxi” anticipa de alguna manera la condición de boxeador del animal.\r\n\r\nSus colores son el azul y el oro.'),
-(8, 'Ingeniería', 'Olavarría', 'En el año 2016 se largó una encuesta por Facebook, para que los alumnos puedan decidir la misma, en donde la mascota que obtuvo más votos fue un Lobo.\r\n\r\nLos colores con los cuales lo vamos a encontrar son verde oscuro y blanco; éstos pueden ser combinados con azul en ciertas ocasiones, ya que este color forma parte del logo del centro de estudiantes.'),
-(9, 'Quequén', 'Quequén', 'El origen de su mascota es reciente, la misma es un duende. La historia del porqué se eligió esta mascota viene dada porque en los alrededores del predio, donde se encuentra la sede, está rodeado de gnomos.\r\n\r\nTomando de esta misma manera sus colores, un azul marino, medio verdoso, por el mar.'),
-(10, 'Salud', 'Olavarría', 'Se identifican con los colores verde y azul.\r\n\r\nNo tienen mascota por el momento.'),
-(11, 'Sociales', 'Olavarría', 'Sus colores son el naranja y el negro.\r\n\r\nPor el momento no tienen definida una mascota.'),
-(12, 'Veterinarias', 'Tandil', 'Son conocidos como los borrachos del tablón, no tienen una mascota bien definida, pero el resto de las facultades lo representan con un paisano. Por los equipos de futsal femenino y básquet se vienen representando con el chancho.\r\n\r\nSus colores vienen definidos porque mundialmente la cruz violeta es representativa de la veterinaria, junto con el blanco y negro.'),
-(14, 'Olimpiadas', '', 'En el año 1983, cuando terminó el proceso militar, nació el proyecto de las Olimpiadas por iniciativa de los deportistas de Veterinarias y de Económicas, quienes llevaron este planteo a la secretaría de deportes con el fin de incentivar a todas las facultades de las sedes de UNICEN a la actividad deportiva, siendo la ciudad de Tandil el núcleo para desarrollar la misma.\r\n\r\nCon todo este movimiento surgió la primera mascota de las Olimpiadas Intercede llamada “Piada”, a quien le dió su origen la secretaria de deporte de ese momento llamada Carolina.\r\n\r\nLos primeros años estuvieron abocados principalmente a deportes, donde se practicaba canotaje, tiro y las pruebas de atletismo, que se desarrollaban en las pistas con salto en alto y largo. Las distintas alternativas se fueron adaptando a la actualidad para una mayor integración de los estudiantes, quienes mediante su voto fueron generando este cambio.\r\n\r\nEsta organización se sostuvo los primeros años con el trabajo de los profesores, que mantenían toda esta estructura con las recaudaciones que se hacían a través de los bailes olímpicos en el Club Independiente.\r\n\r\nAl pasar los años, este evento empezó a tomar tal magnitud que era imposible para la Secretaría de Deportes sostener su estructura solamente con los fondos originados de los bailes olímpicos. Cuando la Universidad toma conciencia de esta situación busca evitar que se pierda el espacio generado por los estudiantes, considerándola de interés universitario, pasando la misma a ser parte del presupuesto de la UNICEN para su financiación.');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario`
---
-
-CREATE TABLE `usuario` (
-  `id_usuario` int(11) NOT NULL,
-  `email_usuario` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+(15, 'Olimpiadas', '', 'En el año 1983, cuando terminó el proceso militar, nació el proyecto de las Olimpiadas por iniciativa de los deportistas de Veterinarias y de Económicas, quienes llevaron este planteo a la secretaría de deportes con el fin de incentivar a todas las facultades de las sedes de UNICEN a la actividad deportiva, siendo la ciudad de Tandil el núcleo para desarrollar la misma.\r\n\r\nCon todo este movimiento surgió la primera mascota de las Olimpiadas Intercede llamada “Piada”, a quien le dió su origen la secretaria de deporte de ese momento llamada Carolina.\r\n\r\nLos primeros años estuvieron abocados principalmente a deportes, donde se practicaba canotaje, tiro y las pruebas de atletismo, que se desarrollaban en las pistas con salto en alto y largo. Las distintas alternativas se fueron adaptando a la actualidad para una mayor integración de los estudiantes, quienes mediante su voto fueron generando este cambio.\r\n\r\nEsta organización se sostuvo los primeros años con el trabajo de los profesores, que mantenían toda esta estructura con las recaudaciones que se hacían a través de los bailes olímpicos en el Club Independiente.\r\n\r\nAl pasar los años, este evento empezó a tomar tal magnitud que era imposible para la Secretaría de Deportes sostener su estructura solamente con los fondos originados de los bailes olímpicos. Cuando la Universidad toma conciencia de esta situación busca evitar que se pierda el espacio generado por los estudiantes, considerándola de interés universitario, pasando la misma a ser parte del presupuesto de la UNICEN para su financiación.'),
+(16, 'Agronomía', 'Azul', 'Por el momento no tienen una mascota definida.\r\n\r\nLos colores que utilizan son el bordó y el blanco.'),
+(17, 'Arte', 'Tandil', 'Adoptaron como mascota al arlequín.\r\n\r\nSus colores varían entre las tonalidades del rojo. Sus camisetas suelen ser color azul violáceo y rojo.'),
+(18, 'Derecho', 'Azul', 'Esta facultad si bien no tiene del todo definida su mascota, el logo de las remeras de sus equipos es un cuervo. Los jugadores decidieron poner esta mascota porque a los abogados se los suele identificar con ese animal.\r\n\r\nLos colores con lo que los vas a poder identificar son el magenta y el azul.'),
+(19, 'Económicas', 'Tandil', 'Según cuenta la leyenda, los actuales contadores Gabriel Dadiego y Mario Cabitto fueron designados para establecer el apodo del Centro de estudiantes y los colores que se usarían en olimpiadas.\r\n\r\nEllos, hinchas de Estudiantes de La Plata uno y de Racing Club el otro, hicieron ostentación de esa condición para elegir el apodo de \"León\" y los colores celeste y blanco.'),
+(20, 'Exactas', 'Tandil', 'Según el boca en boca, la facultad de Exactas es representada por el gallo debido a que hace mucho tiempo, veterinarias hizo una canción llamándoles gallinas y ellos les retrucaron con otra canción autoproclamándose gallos, alegando a la fiereza combativa de dicho animal.\r\n\r\nEn cuanto a los colores, la historia se remonta a las primeras olimpiadas donde no existía ningún tipo de presupuesto para las remeras. Ante esto, el Club Santamarina les prestó remeras suyas de color oro y negro.'),
+(21, 'Humanas', 'Tandil', 'Su mascota es Boxitracio, un personaje inventado por García Ferré para la serie Hijitus. El prefijo “Boxi” anticipa de alguna manera la condición de boxeador del animal.\r\n\r\nSus colores son el azul y el oro.'),
+(22, 'Ingeniería', 'Olavarria', 'En el año 2016 se largó una encuesta por Facebook, para que los alumnos puedan decidir la misma, en donde la mascota que obtuvo más votos fue un Lobo.\r\n\r\nLos colores con los cuales lo vamos a encontrar son verde oscuro y blanco; éstos pueden ser combinados con azul en ciertas ocasiones, ya que este color forma parte del logo del centro de estudiantes.'),
+(23, 'Quequén', 'Quequén', 'EL origen de su mascota es reciente, la misma es un duende. La historia del porqué se eligió esta mascota viene dada porque en los alrededores del predio, donde se encuentra la sede, está rodeado de gnomos.\r\n\r\nTomando de esta misma manera sus colores, un azul marino, medio verdoso, por el mar.'),
+(24, 'Salud', 'Olavarría', 'Se identifican con los colores verde y azul.\r\n\r\nNo tienen mascota por el momento.'),
+(25, 'Sociales', 'Olavarría', 'Sus colores son el naranja y el negro.\r\n\r\nPor el momento no tienen definida una mascota.'),
+(26, 'Veterinarias', 'Tandil', 'Son conocidos como los borrachos del tablón, no tienen una mascota bien definida, pero el resto de las facultades lo representan con un paisano. Por los equipos de futsal femenino y básquet se vienen representando con el chancho.\r\n\r\nSus colores vienen definidos porque mundialmente la cruz violeta es representativa de la veterinaria, junto con el blanco y negro.');
 
 --
 -- Índices para tablas volcadas
@@ -159,12 +147,6 @@ ALTER TABLE `facultad`
   ADD PRIMARY KEY (`id_facultad`) USING BTREE;
 
 --
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuario`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -173,35 +155,11 @@ ALTER TABLE `usuario`
 --
 ALTER TABLE `alumno`
   MODIFY `id_alumno` int(11) NOT NULL AUTO_INCREMENT;
-
 --
 -- AUTO_INCREMENT de la tabla `facultad`
 --
 ALTER TABLE `facultad`
-  MODIFY `id_facultad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `alumno`
---
-ALTER TABLE `alumno`
-  ADD CONSTRAINT `alumno_ibfk_1` FOREIGN KEY (`id_alumno`) REFERENCES `facultad` (`id_facultad`);
-
---
--- Filtros para la tabla `copas`
---
-ALTER TABLE `copas`
-  ADD CONSTRAINT `fk_copas_facultad` FOREIGN KEY (`ganador`) REFERENCES `facultad` (`id_facultad`);
-COMMIT;
+  MODIFY `id_facultad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
